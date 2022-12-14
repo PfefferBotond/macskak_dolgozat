3c89969ee96466fe1bb18d6ec4e5d42dc1418443
PfefferBotond <botond.pfeffer@gmail.com>
2022-12-14 13:54:35 +0100

    Macsakak listazasa

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,10 @@
 import {
+  Body,
   Controller,
   Get,
+  Post,
+  Query,
+  Redirect,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
@@ -8,11 +12,37 @@ import db from './db';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  getHello(): any {
+    throw new Error('Nincs hozzáadva');
+  }
+  constructor(private appService: AppService) {}
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async index(@Query('szem_szin') szem_szin: string) {
+    if (szem_szin != undefined) {
+      const [rows] = await db.execute(
+        'SELECT szem_szn, suly FROM macskak WHERE szem_szín = ?',
+        [szem_szin],
+      );
+
+      return {
+        macskak: rows,
+      };
+    } else {
+      const [rows] = await db.execute(
+        'SELECT szem_szín, suly FROM macskak ORDER BY suly DESC',
+      );
+
+      return {
+        macskak: rows,
+      };
+    }
+  }
+
+  @Get('cats/new')
+  @Render('form')
+  newMacskaForm() {
+    return {};
   }
 }
